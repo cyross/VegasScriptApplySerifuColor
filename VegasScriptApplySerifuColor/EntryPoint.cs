ac61da900c6cbac6ee5ef6f2ebe0e86dcc75372a
Cyross Makoto <takuan45189@gmail.com>
2023-01-22 17:38:36 +0900

    first commit

---
+++ VegasScriptApplySerifuColor/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using ScriptPortal.Vegas;
+using System.Windows.Forms;
+
+namespace VegasScriptApplySerifuColor
+{
+    public class EntryPoint
+    {
+        public void FromVegas(Vegas vegas)
+        {
+            VegasScriptSettings.Load();
+            VegasHelper helper = VegasHelper.Instance(vegas);
+
+            TrackEvents events = helper.GetVideoEvents();
+            if (events is null)
+            {
+                MessageBox.Show("ビデオトラックが選択されていません。");
+                return;
+            }
+            if (events.Count == 0)
+            {
+                MessageBox.Show("選択したビデオトラック中にイベントが存在していません。");
+            }
+
+            helper.ApplyTextColorByActor(events);
+        }
+    }
+}
