443d74aecdfae4b29972c33f6b9ce37277b39097
Cyross Makoto <takuan45189@gmail.com>
2023-02-04 20:18:15 +0900

    IEntryPointインタフェースの実装

--- VegasScriptApplySerifuColor/EntryPoint.cs
+++ VegasScriptApplySerifuColor/EntryPoint.cs
@@ -6,7 +6,7 @@ using VegasScriptHelper;
 
 namespace VegasScriptApplySerifuColor
 {
-    public class EntryPoint
+    public class EntryPoint: IEntryPoint
     {
         public void FromVegas(Vegas vegas)
         {
